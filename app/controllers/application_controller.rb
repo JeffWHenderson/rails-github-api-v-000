3e3180040027ee2749eabcfaece06ae8ab6ad19e
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-08 22:16:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,12 @@ class ApplicationController < ActionController::Base
   private
 
     def authenticate_user
+      client_id = ENV['CLIENT_ID']
+      github_authorize = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
+      redirect_to github_authorize unless logged_in?
     end
 
     def logged_in?
+      !!session[:token]
     end
 end
